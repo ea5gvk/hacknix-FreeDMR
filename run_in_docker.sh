3cd285eb8efe4b3185609a0dab770ee2894fe053
Simon <simon@adlem.net>
2021-02-07 22:31:38 +0000

    Docker update

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -3,4 +3,6 @@
 #Run in docker with 50 HBP ports and 5 OBP ports
 #Allow monitor to connect on 4321
 
-docker run --read-only -v ./config/FreeDMR.cfg:/opt/freedmr/FreeDMR.cfg -v ./FreeDMR.log:/opt/freedmr/FreeDMR.log -v ./config/rules.py:/opt/freedmr/rules.py -p 54000-54050:54000-54050/udp -p 64036-64040:64036-64040/udp -p 4321:4321/tcp freedmr
+
+docker run --read-only -d  -v `pwd`/config/freedmr-docker.cfg:/opt/freedmr/hblink.cfg -v /tmp/FreeDMR.log:/opt/freedmr/freedmr.log -v `pwd`/config/rules.py:/opt/freedmr/rules.py -p 54000-54050:54000-54050/udp -p 62036-62046:62036-62046/udp -p 4321:4321/tcp freedmr
+
