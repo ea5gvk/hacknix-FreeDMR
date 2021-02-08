6184fa08b5a65b03f1204d12685b2088f0efd774
Simon <simon@adlem.net>
2021-02-08 17:11:48 +0000

    Added silence mapping for "to" in most languages

--- i8n_voice_map.py
+++ i8n_voice_map.py
@@ -5,10 +5,35 @@ voiceMap = {
     
     'es_ES': {
                 'to': 'silence'
-    }
+    },
     
     'fr_FR': {
                 'to': 'silence'
-    }
+    },
     
+    'de_DE': {
+                'to': 'silence'
+    },
+    
+    'dk_DK': {
+                'to': 'silence'
+    },
+    
+    'it_IT': {
+                'to': 'silence'
+    },
+    
+    'no_NO': {
+                'to': 'silence'
+    },
+    
+    'pl_PL': {
+                'to': 'silence'
+    },
+    
+    'se_SE': {
+                'to': 'silence'
+    }
+
 }
+    
