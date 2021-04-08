7f4c6f5375b589aa78e74f0002448afe95a625a2
Simon <simon@gb7fr.org.uk>
2021-04-08 18:35:08 +0100

    c

--- bridge_master.py
+++ bridge_master.py
@@ -1072,6 +1072,12 @@ class routerOBP(OPENBRIDGE):
                     #We want to ignore this system and TS combination if it's called again for this packet
                     _sysIgnore.append((_target['SYSTEM'],_target['TS']))
         
+                    if ('_bcsq' in _target_system) and (_dst_id in _target_system['_bcsq']) and (_target_system['_bcsq'][_dst_id] == _stream_id)):
+                        logger.info('(%s) Conference Bridge: %s, is Source Quenched for Stream ID: %s, skipping system: %s TS: %s, TGID: %s', self._system, _bridge, int_id(_stream_id), _target['SYSTEM'], _target['TS'], int_id(_target['TGID']))
+                        continue
+                        
+                        
+                    
                     # Is this a new call stream on the target?
                     if (_stream_id not in _target_status):
                         # This is a new call stream on the target
@@ -1312,6 +1318,7 @@ class routerOBP(OPENBRIDGE):
                 #if _bridge[0:1] != '#':
                 #if True:
                     for _system in BRIDGES[_bridge]:
+                        
                         if _system['SYSTEM'] == self._system and _system['TGID'] == _dst_id and _system['TS'] == _slot and _system['ACTIVE'] == True:
                             _sysIgnore = self.to_target(_peer_id, _rf_src, _dst_id, _seq, _slot, _call_type, _frame_type, _dtype_vseq, _stream_id, _data, pkt_time, dmrpkt, _bits,_bridge,_system,False,_sysIgnore)
 
